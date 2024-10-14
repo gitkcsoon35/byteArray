0bceca9b3f7c394804de0b51574aa8fb406045a9
KimChangSoo <cskim@pentasecurity.com>
2024-10-14 12:03:10 +0900

    [develop-b] second Commit - Hello world2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,9 +21,8 @@ public class Main {
             byteArray("C:\\Users\\P21911001\\Downloads\\licenseUpdate_55.eci");
             byteArray("C:\\Users\\P21911001\\Downloads\\licenseUpdate2_55.eci");
             System.out.println("Hello world!");
-            System.out.println("Hello world!");
-            System.out.println("Hello world!");
-            System.out.println("Hello world!");
+            System.out.println("Hello world 2!");
+            System.out.println("Hello world 3!");
 
         } else if (gubun == "2") {
             contractNumber();
